933d751b256611b81620378c76136d77f243044b
fghilmany <ceeros99@gmail.com>
2024-01-23 14:00:17 +0700

    clean code

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -54,18 +54,16 @@ class HomePage extends StatelessWidget {
               children: [
                 TextButton(
                   style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(const Size(80.0, 80.0)),
-                    backgroundColor: MaterialStateProperty.all(ColorUiKit.greenColor),
+                    fixedSize:
+                        MaterialStateProperty.all(const Size(80.0, 80.0)),
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorUiKit.greenColor),
                     shape: MaterialStateProperty.all(
                       const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(
                             Radius.circular(12.0),
                           ),
-                          side: BorderSide(
-                              width: 2.0,
-                              color: Colors.black
-                          )
-                      ),
+                          side: BorderSide(width: 2.0, color: Colors.black)),
                     ),
                   ),
                   onPressed: () {},
@@ -77,18 +75,16 @@ class HomePage extends StatelessWidget {
                 ),
                 TextButton(
                   style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(const Size(80.0, 80.0)),
-                    backgroundColor: MaterialStateProperty.all(ColorUiKit.redColor),
+                    fixedSize:
+                        MaterialStateProperty.all(const Size(80.0, 80.0)),
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorUiKit.redColor),
                     shape: MaterialStateProperty.all(
                       const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(
                             Radius.circular(12.0),
                           ),
-                          side: BorderSide(
-                              width: 2.0,
-                              color: Colors.black
-                          )
-                      ),
+                          side: BorderSide(width: 2.0, color: Colors.black)),
                     ),
                   ),
                   onPressed: () {},
@@ -110,89 +106,82 @@ class HomePage extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-              padding: EdgeInsets.only(top: 24.0, bottom: 16.0),
-              itemCount: 2,
-                itemBuilder: (context, builder){
-              return Row(
-                children: [
-                  Container(
-                    width: 40.0,
-                    height: 40.0,
-                    margin: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: ColorUiKit.greenColor,
-                      borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                      border: Border.all(color: Colors.black, width: 2.0),
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      size: 24,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const Expanded(
-                    flex: 5,
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(14)
+                padding: const EdgeInsets.only(top: 24.0, bottom: 16.0),
+                itemCount: 2,
+                itemBuilder: (context, builder) {
+                  return Row(
+                    children: [
+                      Container(
+                        width: 40.0,
+                        height: 40.0,
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: ColorUiKit.greenColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(12.0)),
+                          border: Border.all(color: Colors.black, width: 2.0),
                         ),
-                        side: BorderSide(
+                        child: const Icon(
+                          Icons.add,
+                          size: 24,
                           color: Colors.black,
-                          width: 2.0
-                        )
-                      ),
-                      margin: EdgeInsets.all(8.0),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Rp 100.00",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0
-                              ),
-                            ),
-                            Text(
-                              "01/01/2023",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12.0
-                              ),
-                            ),
-                            Text(
-                              "Jajan bakso",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14.0
-                              ),
-                            ),
-                          ],
                         ),
                       ),
-                    ),
-                  ),
-                  Container(
-                    width: 40.0,
-                    height: 40.0,
-                    margin: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: ColorUiKit.redColor,
-                      borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                      border: Border.all(color: Colors.black, width: 2.0),
-                    ),
-                    child: const Icon(
-                      Icons.remove,
-                      size: 24,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              );
-            }),
+                      const Expanded(
+                        flex: 5,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(14)),
+                              side:
+                                  BorderSide(color: Colors.black, width: 2.0)),
+                          margin: EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Rp 100.00",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.0),
+                                ),
+                                Text(
+                                  "01/01/2023",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 12.0),
+                                ),
+                                Text(
+                                  "Jajan bakso",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14.0),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 40.0,
+                        height: 40.0,
+                        margin: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: ColorUiKit.redColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(12.0)),
+                          border: Border.all(color: Colors.black, width: 2.0),
+                        ),
+                        child: const Icon(
+                          Icons.remove,
+                          size: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  );
+                }),
           )
         ],
       ),
