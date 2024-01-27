b749c3cc3e7074c78f713f3ca5dfde0729fb53ee
fghilmany <ceeros99@gmail.com>
2024-01-27 21:41:40 +0700

    implement slidable

--- lib/features/transaction_history/ui/widget/list_transaction.dart
+++ lib/features/transaction_history/ui/widget/list_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:simple_financial_records/common/extension/common_extension.dart';
 import 'package:simple_financial_records/features/transaction_history/domain/finance.dart';
 import 'package:simple_financial_records/features/transaction_history/domain/transaction_type.dart';
@@ -13,77 +14,123 @@ class ListTransaction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        if (transaction?.type == TransactionType.income.name) Container(
-          width: 40.0,
-          height: 40.0,
-          margin: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            color: ColorUiKit.greenColor,
-            borderRadius:
-            const BorderRadius.all(Radius.circular(12.0)),
-            border: Border.all(color: Colors.black, width: 2.0),
-          ),
-          child: const Icon(
-            Icons.add,
-            size: 24,
-            color: Colors.black,
-          ),
-        ),
-        Expanded(
-          flex: 5,
-          child: Card(
-            shape: const RoundedRectangleBorder(
-                borderRadius:
-                BorderRadius.all(Radius.circular(14)),
-                side:
-                BorderSide(color: Colors.black, width: 2.0)),
-            margin: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Rp ${transaction?.nominal}",
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0),
-                  ),
-                  Text(
-                    transaction?.date.parseTimeMillisToDate("dd/MM/yyyy") ?? "",
-                    style: const TextStyle(
-                        color: Colors.black, fontSize: 12.0),
-                  ),
-                  Text(
-                    transaction?.notes ?? "",
-                    style: const TextStyle(
-                        color: Colors.black, fontSize: 14.0),
-                  ),
-                ],
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8.0,
+      ),
+      child: Slidable(
+        endActionPane: ActionPane(
+          extentRatio: 0.3,
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              // An action can be bigger than the others.
+              flex: 1,
+              onPressed: (value) {},
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(14.0),
+                  bottomLeft: Radius.circular(14.0)),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: "Delete",
             ),
+          ],
+        ),
+        startActionPane: ActionPane(
+          extentRatio: 0.3,
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              // An action can be bigger than the others.
+              flex: 1,
+              onPressed: (value) {},
+              borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(14.0),
+                  bottomRight: Radius.circular(14.0)),
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: "Delete",
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Row(
+            children: [
+              if (transaction?.type == TransactionType.income.name)
+                Container(
+                  width: 40.0,
+                  height: 40.0,
+                  margin: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: ColorUiKit.greenColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+                    border: Border.all(color: Colors.black, width: 2.0),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                ),
+              Expanded(
+                flex: 5,
+                child: Card(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14)),
+                      side: BorderSide(color: Colors.black, width: 2.0)),
+                  margin: EdgeInsets.zero,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Rp ${transaction?.nominal}",
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0),
+                        ),
+                        Text(
+                          transaction?.date
+                                  .parseTimeMillisToDate("dd/MM/yyyy") ??
+                              "",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 12.0),
+                        ),
+                        Text(
+                          transaction?.notes ?? "",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              if (transaction?.type == TransactionType.outcome.name)
+                Container(
+                  width: 40.0,
+                  height: 40.0,
+                  margin: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: ColorUiKit.redColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+                    border: Border.all(color: Colors.black, width: 2.0),
+                  ),
+                  child: const Icon(
+                    Icons.remove,
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                ),
+            ],
           ),
         ),
-        if (transaction?.type == TransactionType.outcome.name) Container(
-          width: 40.0,
-          height: 40.0,
-          margin: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            color: ColorUiKit.redColor,
-            borderRadius:
-            const BorderRadius.all(Radius.circular(12.0)),
-            border: Border.all(color: Colors.black, width: 2.0),
-          ),
-          child: const Icon(
-            Icons.remove,
-            size: 24,
-            color: Colors.black,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
