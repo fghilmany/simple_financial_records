0a4cf0717e6e8d3c00670bc06007edd68d72937f
fghilmany <ceeros99@gmail.com>
2024-01-28 00:02:24 +0700

    add splash screen

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_financial_records/common/color_ui_kit.dart';
 import 'package:simple_financial_records/features/home/presentation/home_page_bloc.dart';
 import 'package:simple_financial_records/features/transaction_history/presentation/history_transaction_page_bloc.dart';
 import 'package:simple_financial_records/main/factories/history_transaction/home_page_bloc_factory.dart';
@@ -10,8 +12,11 @@ import '../features/home/ui/home_page.dart';
 void main() {
   runApp(MultiBlocProvider(
     providers: [
-      BlocProvider<HomePageBloc>(create: (context) => HomePageBlocFactory.createHomePageBloc()),
-      BlocProvider<HistoryTransactionPageBloc>(create: (context) => HistoryTransactionPageBlocFactory.createHistoryTransactionPageBloc()),
+      BlocProvider<HomePageBloc>(
+          create: (context) => HomePageBlocFactory.createHomePageBloc()),
+      BlocProvider<HistoryTransactionPageBloc>(
+          create: (context) => HistoryTransactionPageBlocFactory
+              .createHistoryTransactionPageBloc()),
     ],
     child: const MyApp(),
   ));
@@ -29,7 +34,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: AnimatedSplashScreen(
+        splash: "image/personal_finance_logo.png",
+        splashIconSize: 256.0,
+        backgroundColor: ColorUiKit.backgroundColor,
+        splashTransition: SplashTransition.decoratedBoxTransition,
+        nextScreen: const HomePage(),
+      ),
     );
   }
 }
